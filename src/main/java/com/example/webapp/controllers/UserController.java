bc144b13c03591eca2c826f1be5c8d81a7fd1520
atsypaleva <tsypalev@outlook.com>
2021-09-09 09:28:19 +0300

    3.1.2

--- src/main/java/com/example/webapp/controllers/UserController.java
+++ src/main/java/com/example/webapp/controllers/UserController.java
@@ -7,11 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.security.Principal;
 
 @Controller
 public class UserController {
@@ -34,34 +31,22 @@ public class UserController {
     public String getCurrentUserInfo(Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("user", user);
-        return "show";
+        return "user";
     }
 
-    @GetMapping("/admin/users")
-    public String showAllUsers(Model model, Principal user) {
+    @GetMapping("/admin")
+    public String showAllUsers(Model model) {
+        User thisUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("newUser", new User());
         model.addAttribute("usersList", userService.getAllUsers());
-        return "users";
-    }
-
-    @GetMapping("/admin/users/{id}")
-    public String showUserById(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("user", userService.getById(id));
-        return "show";
-    }
-
-
-    @GetMapping("/admin/users/new")
-    public String newUser(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("thisUser", thisUser);
         model.addAttribute("allRoles", roleService.getAllRoles());
-        return "new";
+        return "admin";
     }
 
-    @PostMapping("/admin/users")
-    public String createUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+    @PostMapping("/admin/create")
+    public String createUser(@ModelAttribute("newUser") User user,
                              @RequestParam(value = "index", required = false) Long[] index) {
-        if (bindingResult.hasErrors()) {
-            return "new";
-        }
         if (index != null) {
             for (Long id : index) {
                 user.addRole(roleService.findById(id));
@@ -70,23 +55,12 @@ public class UserController {
             user.addRole(roleService.findById(2L));
         }
         userService.save(user);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 
-    @GetMapping("/admin/users/{id}/edit")
-    public String editUser(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("allRoles", roleService.getAllRoles());
-        model.addAttribute("user", userService.getById(id));
-        return "edit";
-    }
-
-    @PostMapping("/admin/users/{id}")
-    public String updateUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
-                             @RequestParam(value = "index", required = false) Long[] index) {
-        if (bindingResult.hasErrors()) {
-            return "edit";
-        }
-
+    @PostMapping("/admin/update")
+    public String update(@ModelAttribute("newUser") User user,
+                         @RequestParam(value = "index", required = false) Long[] index) {
         if (index != null) {
             for (Long id : index) {
                 user.addRole(roleService.findById(id));
@@ -95,12 +69,12 @@ public class UserController {
             user.addRole(roleService.findById(2L));
         }
         userService.update(user);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 
-    @PostMapping("/admin/users/{id}/delete")
-    public String deleteUser(@PathVariable("id") Long id) {
+    @PostMapping("/admin/delete")
+    public String deleteUser(@RequestParam("id") Long id) {
         userService.delete(id);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 }
